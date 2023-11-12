ea11832fd2d1a052530fd7856e0d52c1c33745ec
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-11-12 14:05:55 +0530

    PermutationVowelsTogether

---
+++ permutationVowelsTogether.py
@@ -0,0 +1,17 @@
+import math
+letter = input("Enter letters: ")
+vowels = ''
+vow = []
+non = ''
+
+for i in range(0,len(letter)):
+    if (letter[i] in ['a','e','i','o','u']):
+        vowels = vowels + letter[i]        
+    else:
+        non = non + letter[i]
+vow.append(vowels)
+total_fact = len(vow) + len(non)
+vowels_fact = len(vowels)
+permutation = (math.factorial(total_fact) * math.factorial(vowels_fact))
+print("permutation vowels together :")
+print(total_fact,"! ",' x ',vowels_fact,"! ",'= ',(math.factorial(total_fact) *math.factorial(vowels_fact)))
